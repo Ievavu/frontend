72a8c2e03c1a2d8451672af8b17351886264acd8
Ieva Vidunaite-Urbaniene <i.vidunaite@gmail.com>
2023-07-08 15:47:50 +0300

    added login page

---
+++ src/pages/login/index.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import styles from "./styles.module.css";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setErrorMessage('');
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setErrorMessage('');
+  };
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      setErrorMessage('Please fill in all fields.');
+      return;
+    }
+
+    if (email.indexOf('@') === -1) {
+      setErrorMessage('Invalid email format.');
+      return;
+    }
+
+    try {
+      const response = await axios.post('http://localhost:8081/login', {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        // Store JWT in cookies or local storage
+        document.cookie = `jwt=${response.data.jwt}; path=/`;
+
+        // Redirect to another page
+        window.location.href = '/';
+      }
+    } catch (error) {
+      setErrorMessage('Login failed. Please try again.');
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Login</h2>
+      <div className={styles.form}>
+        <div className={styles.formGroup}>
+          <label htmlFor="email" className={styles.label}>
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="password" className={styles.label}>
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            className={styles.input}
+          />
+        </div>
+        {errorMessage && <p className={styles.error}>{errorMessage}</p>}
+        <button className={styles.button} onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
