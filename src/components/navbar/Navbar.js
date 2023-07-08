0612b2efca89d121ef946ce021d40707c24558ed
Ieva Vidunaite-Urbaniene <i.vidunaite@gmail.com>
2023-07-08 16:44:25 +0300

    Made logo a link to main page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,18 @@
 import React from "react";
 import styles from "./styles.module.css";
 import logo from "../../assets/logo.png";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <div>
-        <img className={styles.logo} src={logo.src} />
+        <Link href="/">
+          <img className={styles.logo} src={logo.src} alt="Logo" />
+        </Link>
       </div>
 
       <ul>
-        <li>
-          <a className={styles.button} href="/">Main</a>
-        </li>
         <li>
           <a className={styles.button} href="/newQuestion">Ask a question</a>
         </li>
